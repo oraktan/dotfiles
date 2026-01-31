13cb4ae07593abf1e511e2b2d1cc27ee7497a0a7
oraktan <tnrork@gmial.com>
2026-01-31 19:00:04 +0300

    yazi updated

--- yazi/.config/yazi/init.lua
+++ yazi/.config/yazi/init.lua
@@ -1,2 +1,9 @@
 require("full-border"):setup()
 -- Eğer eklenti hata verirse bu satırı silip öyle dene
+-- Git desteğini aktif et (git.yazi)
+require("git"):setup()
+
+-- Tam ekran kenarlıkları (full-border.yazi)
+
+-- Durum çubuğuna (status bar) daha fazla detay ekler
+require("git"):setup()
