2c74897b13997faf3c7ae2afbe243b5d89b7a86b
oraktan <tnrork@gmial.com>
2026-03-08 01:19:04 +0300

    8 mart gecesi yaptığım güncellemeler

--- hypr/.config/hypr/UserScripts/RofiCalc.sh
+++ hypr/.config/hypr/UserScripts/RofiCalc.sh
@@ -1,30 +1,38 @@
 #!/usr/bin/env bash
-# /* ---- 💫 https://github.com/JaKooLit 💫 ---- */
-# /* Calculator (using qalculate) and rofi */
-# /* Submitted by: https://github.com/JosephArmas */
 
-rofi_theme="$HOME/.config/rofi/config-calc.rasi"
-
-# Kill Rofi if already running before execution
-if pgrep -x "rofi" >/dev/null; then
-    pkill rofi
+# Rofi temasını seç (dosya yoksa standart temayı kullanır)
+if [ -f "$HOME/.config/rofi/config-calc.rasi" ]; then
+    rofi_theme="$HOME/.config/rofi/config-calc.rasi"
+else
+    rofi_theme="$HOME/.config/rofi/config.rasi"
 fi
 
-# main function
+# Rofi zaten açıksa kapat
+pkill rofi
 
 while true; do
-    result=$(
-        rofi -i -dmenu \
-            -config $rofi_theme \
-            -mesg "$result      =    $calc_result"
-    )
+    # Kullanıcıdan giriş al
+    # -p "Hesapla": Sol üstte görünür
+    # -mesg: Sonucu bir alt satırda gri tonda gösterir
+    user_input=$(rofi -dmenu \
+        -config "$rofi_theme" \
+        -p "󰪚 Hesapla" \
+        -mesg "Sonuç: $calc_result")
 
-    if [ $? -ne 0 ]; then
+    # Esc basılırsa veya boş bırakılırsa çık
+    if [ $? -ne 0 ] || [ -z "$user_input" ]; then
         exit
     fi
 
-    if [ -n "$result" ]; then
-        calc_result=$(qalc -t "$result")
-        echo "$calc_result" | wl-copy
+    # HESAPLAMA MOTORU (qalc kullanıyoruz)
+    # -t: Sadece sonucu verir (gereksiz yazıları siler)
+    calc_result=$(qalc -t "$user_input" 2>/dev/null)
+
+    # Eğer hesaplama başarılıysa
+    if [ -n "$calc_result" ]; then
+        # Sonucu panoya kopyala
+        echo -n "$calc_result" | wl-copy
+    else
+        calc_result="Hata! Geçersiz işlem."
     fi
 done
